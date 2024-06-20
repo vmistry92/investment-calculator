4ae26d82b803174bce32ab1f0d72cbc0a8ab0fe5
Vinay Mistry <vinay@vinaymistry.com>
2024-06-20 10:52:29 +0100

    feat: user input component with two way binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import UserInput from "./components/UserInput/UserInput";
 
 function App() {
   return (
     <>
       <Header />
+      <UserInput />
     </>
   );
 }
